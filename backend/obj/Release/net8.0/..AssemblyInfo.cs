7a472678c1dff7b1d24a4228bd9732dd87a6eff6
f.maddaluno <f.maddaluno@3em.it>
2025-06-03 12:44:29 +0200

    add BE tools

--- backend/obj/Release/net8.0/..AssemblyInfo.cs
+++ backend/obj/Release/net8.0/..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e5d64d1514c15ddbde0768f3c192390d52b44e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2338f36b114b97f62def52034900da26fc36a42")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
