51cb048a8b0432eab6dff6c51309eb632d9693d2
MicheleSchiano <m.schianodicola@3em.it>
2025-06-03 15:14:45 +0200

    identity EF core package

--- backend/obj/Debug/net8.0/..AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a472678c1dff7b1d24a4228bd9732dd87a6eff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b7784e1bfc21b6db72a3a652b223b71350ba90")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
