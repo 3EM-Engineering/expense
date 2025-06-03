f387f831822483e5b097ceffb5cc9669dd7c728d
Michela <m.guardascione@3em.it>
2025-06-03 12:09:58 +0200

    delete 9.0 be

--- backend/obj/Release/net8.0/..AssemblyInfo.cs
+++ backend/obj/Release/net8.0/..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e5d64d1514c15ddbde0768f3c192390d52b44e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709b99a02ee60de6a60e0e95de9f59b2a75347a3")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
