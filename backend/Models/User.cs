3a574c4bdd842f3e91a802eb8e937963b4d066a0
MicheleSchiano <m.schianodicola@3em.it>
2025-06-04 11:12:55 +0200

    creation AppDbContext, User, creation envoirnment variables

---
+++ backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace backend.Models
+{
+    public class User : IdentityUser
+    {
+        //[NotMapped]
+        //public string Role { get; set; }
+    }
+}
