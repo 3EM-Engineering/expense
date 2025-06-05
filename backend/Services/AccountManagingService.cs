0134a5eab83671afa17cb0fea0bc6219db0dddd9
MicheleSchiano <m.schianodicola@3em.it>
2025-06-05 09:57:52 +0200

    create AccountManaginService

---
+++ backend/Services/AccountManagingService.cs
@@ -0,0 +1,36 @@
+﻿//using backend.Dtos.AccountManagingDtos;
+//using backend.Models;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace backend.Services
+//{
+//    public class AccountManagingService
+//    {
+//        private RoleManager<User> _roleManager;
+//        private UserManager<User> _userManager;
+
+//        public AccountManagingService(
+//                RoleManager<User> roleManager,
+//                UserManager<User> userManager
+//            )
+//        {
+//            _roleManager = roleManager;
+//            _userManager = userManager;
+//        }
+
+//        //public async Task<IActionResult> Login()
+//        //{
+
+//        //}
+
+//        public async Task<IActionResult> Registration(RegisterDto obj)
+//        {
+            
+//        }
+
+//        //public async Task<IActionResult> Logout()
+//        //{
+
+//        //}
+//    }
